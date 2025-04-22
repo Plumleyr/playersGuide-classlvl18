f3322a6776ae9f5e0cfccefd9d0b60d7ee1b74b6
jrplu <jrplu@JACOBSPC>
2025-04-21 22:00:57 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,113 @@
+﻿using System.Reflection;
+
+Arrow currentArrow = new Arrow();
+
+currentArrow.arrowhead = WriteEnumOptions<Arrowheads>("Welcome to Vin Fletcher's Arrows. Let's start off with picking the arrowhead.");
+currentArrow.fletching = WriteEnumOptions<Fletching>("Next let's pick a fletching for your arrow to be!");
+
+Console.WriteLine("Finally let's pick a length for that arrow! Enter an integer in range of 60 - 100");
+
+while(currentArrow.shaftLength == 0)
+{
+    if(int.TryParse(Console.ReadLine(), out int choice))
+    {
+        if(choice > 100 || choice < 60)
+        {
+            Console.WriteLine($"{choice} is not in the range 60 - 100");
+        }
+        else
+        {
+            currentArrow.shaftLength = choice;
+        }
+    }
+    else
+    {
+        Console.WriteLine("Please enter a valid integer!");
+    }
+} 
+
+Console.WriteLine($"Your {currentArrow.shaftLength}cm arrow with a {currentArrow.arrowhead} arrowhead and {currentArrow.fletching} fletching will be ${currentArrow.GetCost():F2}");
+
+T WriteEnumOptions<T>(string prompt) where T : Enum
+{
+    Console.WriteLine(prompt);
+
+    T[] enumOptions = (T[])Enum.GetValues(typeof(T));
+
+    for (int i = 0; i < enumOptions.Length; i++)
+    {
+        Console.WriteLine($"{i}: {enumOptions[i]}");
+    }
+
+    int choice;
+    bool validResponse = false;
+
+    do
+    {
+        if (!int.TryParse(Console.ReadLine(), out choice))
+        {
+            Console.WriteLine($"Enter an integer 0 to {enumOptions.Length - 1}.");
+        }
+        else if (choice >= enumOptions.Length || choice < 0)
+        {
+            Console.WriteLine($"{choice} is not in the range of 0 to {enumOptions.Length - 1}");
+        }
+        else
+        {
+            validResponse = true;
+        }
+
+    } while (!validResponse);
+
+    return enumOptions[choice];
+}
+
+class Arrow
+{
+    public Arrowheads arrowhead = default;
+    public Fletching fletching = default;
+    public int shaftLength = 0;
+
+    public float GetCost()
+    {
+        float totalCost = 0;
+
+        FieldInfo[] fields = this.GetType().GetFields();
+
+        foreach (var field in fields)
+        {
+            var value = field.GetValue(this);
+
+            if (value is Arrowheads arrowheadValue)
+            {
+                totalCost += (float)arrowhead;
+            }
+            else if (value is Fletching fletchingValue)
+            {
+                totalCost += (float)fletchingValue;
+            }
+            else if (value is int shaftLengthValue)
+            {
+                totalCost += shaftLengthValue * 0.05F;
+            }
+        }
+
+        Console.WriteLine(fields.Length);
+
+        return totalCost;
+    }
+}
+
+enum Arrowheads
+{
+    Steel = 10,
+    Obsidian = 5,
+    Wood = 3
+}
+
+enum Fletching
+{
+    Plastic = 10,
+    Turkey = 5,
+    Goose = 3
+}
